16d22e46bf072e2c918446acc2fa1af66cd42c79
Smit Patel <smit.patel@tssconsultancy.com>
2026-04-25 20:42:02 +0530

    feat: starting with rule engine

---
+++ src/main/java/org/tss/tm/repository/ScenarioRepo.java
@@ -0,0 +1,10 @@
+package org.tss.tm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tss.tm.entity.system.Scenario;
+
+import java.util.UUID;
+
+public interface ScenarioRepo extends JpaRepository<Scenario, UUID> {
+
+}
